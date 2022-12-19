afcb255e355cebbc8e54b1725c1a8c956ef3c567
Lytsikas, Christos <christos.lytsikas@arhs-dev-hellas.com>
2022-12-19 10:33:07 +0200

    add computations for 11 cards in main

--- src/main/java/org/lytsiware/combinatoric/Main.java
+++ src/main/java/org/lytsiware/combinatoric/Main.java
@@ -18,7 +18,9 @@ public class Main {
                 .filter(s -> subArrayContains(s, (short) 1, 7))
                 .filter(s -> subArrayContains(s, (short) 2, 9))
                 .count();
-        var bothAt4thTurn = sampleSpace.stream().filter(s -> subArrayContains(s, (short) 1, 7)).filter(s -> subArrayContains(s, (short) 2, 7)).count();
+        var bothAt4thTurn = sampleSpace.stream()
+                .filter(s -> subArrayContains(s, (short) 1, 7))
+                .filter(s -> subArrayContains(s, (short) 2, 7)).count();
 
         System.out.println("P (12,9)  = " + sampleSpace.size());
         System.out.println("(a) card at most 4th turn = " + cardAtMost4thTurn);
@@ -26,7 +28,25 @@ public class Main {
         System.out.println("a and b  = " + both);
         System.out.println(bothAt4thTurn);
 
-        System.out.println("a and b as lowest Fraction: = " + NumberUtils.reductFractionToLowestTerms(both, sampleSpace.size()).asSingleTermFraction());
+        System.out.println("a and b as lowest Fraction: = " + NumberUtils.reductFractionToLowestTerms(both, sampleSpace.size()).asSingleTermFraction()
+                + " = " + both / (float) sampleSpace.size());
+
+
+        var sampleSpace2 = new VariationService().generateVariations(new short[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}, 8).toList();
+        var cardAtMost4thTurn2 = sampleSpace2.stream().filter(s -> subArrayContains(s, (short) 1, 7)).count();
+        var cardAtMost6thTurn2 = sampleSpace2.stream().filter(s -> subArrayContains(s, (short) 2, 8)).count();
+        var both2 = sampleSpace2.stream()
+                .filter(s -> subArrayContains(s, (short) 1, 7))
+                .filter(s -> subArrayContains(s, (short) 2, 8))
+                .count();
+
+
+        System.out.println("P (11,8)  = " + sampleSpace2.size());
+        System.out.println("(a) card at most 4th turn = " + cardAtMost4thTurn2);
+        System.out.println("(b) card at most 6th turn = " + cardAtMost6thTurn2);
+        System.out.println("a and b  = " + both2);
+        System.out.println("a and b as lowest Fraction: = " + NumberUtils.reductFractionToLowestTerms(both2, sampleSpace2.size()).asSingleTermFraction()
+                + " = " + both2 / (float) sampleSpace2.size());
     }
 
 
